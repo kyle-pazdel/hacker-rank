48e2cf1d8eafae3a00fa57c8f401e7240eea909d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-11 10:28:47 -0800

    Practice problem 'Mini-max-sum' (Pseudo-code only)

---
+++ mini_max_sum.rb
@@ -0,0 +1,30 @@
+# Add values 0 - 3 together and print the sum on one line
+# Add values 1 - 4 together and print the sum on one line
+# Print first and second sum together on one line
+# set a variable to max_sum equal to 0
+# set a variable to min_sum equal to 0
+# set an index to 0
+# add all numbers except the index together and assign it to min sum
+# set index to 1
+# add all numbers except the index together
+# write conditional, if new total is greater than min sum, set it to max_sum
+# if new total is less than min sum, assign min_sum to max_sum and assign new total to min_sum
+# set index to 2
+# add all numbers except the index together
+# write conditional, if new total is greater than max sum, set it to max_sum
+# if new total is less than min sum, set it to min_sum
+# set an index to 0
+# set a variable to max_sum qual to 0
+# set a variable to min_sum equal to 0
+# iterate with a while loop for one iteration
+# within the loop add all numbers except the index together
+# set the sum to min_sum
+# set the sum to max_sum
+# set index to 1
+# write another while loop to iterate over the remaining length of the array
+# add all numbers except the index together
+# write a conditional, if the sum is less than min_sum, set it to min_sum
+# if the sum is greater than max_sum, set it to max_sum
+# print the min and max sum values on one line
+
+arr = [1, 2, 3, 4, 5]
