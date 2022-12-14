9c3d3c779602d51bf8cbc67d170b373615f9fd9d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-14 05:53:56 -0800

    Practice Problem 'Time conversion (SOLVED)'

--- time_conversion.rb
+++ time_conversion.rb
@@ -2,35 +2,42 @@
 # Write condition to add 12 to the first two digits of s if P
 # assign all except for AM/PM to m variable
 
-s = "07:05:45AM"
+# s = "07:05:45AM"
+s = "12:45:54PM"
+# s = "12:45:54AM"
+# s = "06:40:03AM"
 # s = "12:45:54PM"
-p s[0]
-p s[1]
+
 is_pm = false
-if s[-2] == "P" && s[0] != "1" && s[1] != 2
+if s[-2] == "P"
   is_pm = true
 end
-p is_pm
-if is_pm == true
-  s = s.split("")
-  s[1] = s[1].to_i + 12
+new_hour = s[0] + s[1]
+s = s.split("")
+n = []
+if is_pm == true && new_hour != "12"
+  new_hour = new_hour.to_i + 12
   s[1] = s[1].to_s
-  n = []
-  i = 1
+  i = 2
+  n << new_hour
   while i < s.length - 2
     n << s[i]
     i += 1
   end
-  n = n.join
-  p n
+elsif is_pm == false && s[-2] == "A" && new_hour == "12"
+  new_hour = "00"
+  i = 2
+  n << new_hour
+  while i < s.length - 2
+    n << s[i]
+    i += 1
+  end
 else
-  s = s.split("")
-  n = []
   i = 0
   while i < s.length - 2
     n << s[i]
     i += 1
   end
-  n = n.join
 end
+n = n.join
 p n
