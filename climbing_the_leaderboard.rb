9f940b6d4e32fc60512fab0a14fe906d5892661c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-19 16:00:28 -0800

    Adds pseudo code for climbing_the_leaderboard.rb (WIP)

---
+++ climbing_the_leaderboard.rb
@@ -0,0 +1,7 @@
+# shovel scores into ranked
+# remove duplicates from ranked array
+# sort ranked and reverse ranked array
+# iterate through the player array
+# search for the current index of the player array in the ranked array and return the index + 1
+
+# arr.sort_by{|a,b| b <=> a}
