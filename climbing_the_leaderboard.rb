79ff4ac806cf8b98b9411901683deaf85b99cbcf
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-12 18:32:12 -0800

    Practice problem 'Climbing the Leaderboard' (Solved, but reworking for better efficiency)

--- climbing_the_leaderboard.rb
+++ climbing_the_leaderboard.rb
@@ -10,10 +10,11 @@
 
 # https://apidock.com/ruby/Array/find_index
 
-n = 6
-ranked = [100, 90, 90, 80, 75, 60]
-m = 5
-player = [50, 65, 77, 90, 102]
+# ranked = [100, 90, 90, 80, 75, 60]
+# player = [50, 65, 77, 90, 102]
+
+player = [5, 25, 50, 120]
+ranked = [100, 100, 50, 40, 40, 20, 10]
 
 ranked_short = []
 i = 0
@@ -24,23 +25,64 @@ while i < ranked.length - 1
   i += 1
 end
 ranked_short << ranked[-1]
-p ranked_short
 
-ranked_short = [100, 90, 80, 75, 60]
-player = [50, 65, 77, 90, 102]
+ranked = [100, 50, 40, 20, 10]
+p ranked_short
 
 ranked_short_length = ranked_short.length
 i = 0
 player_rankings = []
-while i < ranked_short_length - 1
+while i < player.length
   rank = ranked_short_length
   score = player[i]
   j = ranked_short_length - 1
-  while rank >= 0 && score >= ranked_short[j]
+  while rank > 0 && score >= ranked_short[j]
+    p "ranked_short[j]: #{ranked_short[j]}"
+    p "score: #{score}"
+    p "rank: #{rank + 1}"
     rank -= 1
     j -= 1
   end
+  p "j: #{j}"
+  p "rank: #{rank}"
+  p "Final Rank: #{rank + 1}"
+  p "________________________________________"
   player_rankings << rank + 1
   i += 1
-  p player_rankings
 end
+p player_rankings
+
+# ranked_short_length = ranked_short.length
+# i = 0
+# player_rankings = []
+# while i < ranked_short_length
+#   rank = ranked_short_length
+#   score = player[i]
+#   j = ranked_short_length - 1
+#   while rank > 0 && score >= ranked_short[j]
+#     rank -= 1
+#     j -= 1
+#   end
+#   p "ranked_short[j]: #{ranked_short[j]}"
+#   p "score: #{score}"
+#   p "rank: #{rank}"
+#   player_rankings << rank + 1
+#   i += 1
+#   p player_rankings
+# end
+
+# ranked_short_length = ranked_short.length
+# i = 0
+# player_rankings = []
+# while i < ranked_short_length - 1
+#   rank = ranked_short_length
+#   score = player[i]
+#   j = ranked_short_length - 1
+#   while rank >= 0 && score >= ranked_short[j]
+#     rank -= 1
+#     j -= 1
+#   end
+#   player_rankings << rank + 1
+#   i += 1
+#   p player_rankings
+# end
