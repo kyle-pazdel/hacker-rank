6c3b2cc51efc61dfb07d34c471a4df794d28a5ea
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-20 06:12:50 -0800

    Practice problem 'Forming a Magic Square' Adds some psuedo code towards solution

---
+++ forming_a_magic_square.rb
@@ -0,0 +1,8 @@
+# # FOR CALCULATING COST:
+# Cost is the absolute value of the difference of all numbers and what they are changed with.
+# Subtract new number from old humber, then repeat for all exchanges
+# add all differences together and find the absolute value of that number to calculate cost of changes.
+
+array = [4 9 2
+  3 5 7
+  8 1 5]
