c113d2d94f1bb2766d498a339db63b9fa06c142a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-02 07:52:59 -0800

    Practice problem 'Forming a Magic Square' (SOLVED)

--- forming_a_magic_square.rb
+++ forming_a_magic_square.rb
@@ -10,12 +10,23 @@
 # Find the index of each duplicate
 # After checking for missing numbers and duplicates, use the index to find which tow, column and diagonal the duplicates and missing numbers are in
 
+# SECOND SOLUTION
+# Find indeces of all duplicates
+# Then add cols, rows, and diagonals. If one of them does not add up to 15, replace the integer at that index with the corresponding number
+# THEN
+# After making the above work, record which numbers needed to be exchanged and move onto the calculation of the cost step below...
+
 # write a conditional to insert a missing number into the index of a duplicate if that row, column or diagonal does not add up to 9
 
 # # FOR CALCULATING COST:
 # Cost is the absolute value of the difference of all numbers and what they are changed with.
 # Subtract new number from old humber, then repeat for all exchanges
 # add all differences together and find the absolute value of that number to calculate cost of changes.
+
+################################################
+
+# # FINAL SOLUTION
+####################################################
 
 # TEST CASE ONE
 # s = [[4, 9, 2],
@@ -26,114 +37,156 @@
 #      [3, 5, 7],
 #      [8, 1, 6]]
 
-output = 1
-
 # TEST CASE TWO
-# s = [[4, 8, 2], [4, 5, 7], [6, 1, 6]]
-# s = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
+# s = [[4, 8, 2],
+#      [4, 5, 7],
+#      [6, 1, 6]]
+
+# s = [[4, 9, 2],
+#      [3, 5, 7],
+#      [8, 1, 6]]
 
 # TEST CASE THREE
 s = [[4, 8, 2],
      [4, 5, 7],
      [6, 2, 6]]
 
-row_numbers = {}
-for i in 1..9
-  row_numbers[i] = [i, 0]
-end
-i = 0
-while i < s.length
-  j = 0
-  while j < s.length
-    num = s[i][j]
-    row_numbers[num][1] += 1
-    j += 1
-  end
-  i += 1
-end
-p "row_numbers: #{row_numbers}"
+magic_possible = [
+  [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+  [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+  [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+  [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+  [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+  [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+  [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+  [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+]
 
-col_numbers = {}
-for i in 1..9
-  col_numbers[i] = [i, 0]
-end
-i = 0
-while i < s.length
-  j = 0
-  while j < s.length
-    num = s[j][i]
-    col_numbers[num][1] += 1
-    j += 1
-  end
-  i += 1
-end
-p "col_numbers: #{col_numbers} "
+min_cost = 72
 
-diagonal_numbers = {}
-for i in 1..9
-  diagonal_numbers[i] = [i, 0]
-end
-i = 0
-j = 2
-while i < s.length
-  num = s[i][i]
-  num2 = s[i][j]
-  if i != 1 || j != 1
-    diagonal_numbers[num][1] += 1
-    diagonal_numbers[num2][1] += 1
+def find_min(a, b)
+  if a < b
+    return a
+  else
+    return b
   end
-  j -= 1
-  i += 1
 end
-middle_number = s[1][1]
-diagonal_numbers[middle_number][1] += 1
-p "dia_numbers: #{diagonal_numbers}"
 
-# Check for missing numbers in rows first...
-def missing_numbers(numbers)
-  missing_numbers = []
-  for num in 1..9
-    if numbers[num][1] == 0
-      missing_numbers << numbers[num][0]
+for i in 1..8
+  cost = 0
+  for j in 1..3
+    for k in 1..3
+      cost += (s[j - 1][k - 1] - magic_possible[i - 1][j - 1][k - 1]).abs
     end
   end
-  return missing_numbers
+  min_cost = find_min(min_cost, cost)
 end
 
-# Check for duplicate numbers in rows first...
-def duplicate_numbers(numbers)
-  duplicate_numbers = []
-  for num in 1..9
-    if numbers[num][1] == 2
-      duplicate_numbers << numbers[num][0]
-    end
-  end
-  return duplicate_numbers
-end
+p min_cost
+
+## PREVIOUS SOLUTION ATTEMPTS
+##############################################################
+
+# row_numbers = {}
+# for i in 1..9
+#   row_numbers[i] = [i, 0]
+# end
+# i = 0
+# while i < s.length
+#   j = 0
+#   while j < s.length
+#     num = s[i][j]
+#     row_numbers[num][1] += 1
+#     j += 1
+#   end
+#   i += 1
+# end
+# p "row_numbers: #{row_numbers}"
+
+# col_numbers = {}
+# for i in 1..9
+#   col_numbers[i] = [i, 0]
+# end
+# i = 0
+# while i < s.length
+#   j = 0
+#   while j < s.length
+#     num = s[j][i]
+#     col_numbers[num][1] += 1
+#     j += 1
+#   end
+#   i += 1
+# end
+# p "col_numbers: #{col_numbers} "
+
+# diagonal_numbers = {}
+# for i in 1..9
+#   diagonal_numbers[i] = [i, 0]
+# end
+# i = 0
+# j = 2
+# while i < s.length
+#   num = s[i][i]
+#   num2 = s[i][j]
+#   if i != 1 || j != 1
+#     diagonal_numbers[num][1] += 1
+#     diagonal_numbers[num2][1] += 1
+#   end
+#   j -= 1
+#   i += 1
+# end
+# middle_number = s[1][1]
+# diagonal_numbers[middle_number][1] += 1
+# p "dia_numbers: #{diagonal_numbers}"
+
+# # Check for missing numbers in rows first...
+# def missing_numbers(numbers)
+#   missing_numbers = []
+#   for num in 1..9
+#     if numbers[num][1] == 0
+#       missing_numbers << numbers[num][0]
+#     end
+#   end
+#   return missing_numbers
+# end
+
+# # Check for duplicate numbers in rows first...
+# def duplicate_numbers(numbers)
+#   duplicate_numbers = []
+#   for num in 1..9
+#     if numbers[num][1] == 2
+#       duplicate_numbers << numbers[num][0]
+#     end
+#   end
+#   return duplicate_numbers
+# end
 
 # p "Missing from rows: #{missing_numbers(row_numbers)}"
-p "Duplicates from rows: #{duplicate_numbers(row_numbers)}"
-row_duplicates = duplicate_numbers(row_numbers)
+# p "Duplicates from rows: #{duplicate_numbers(row_numbers)}"
+# row_duplicates = duplicate_numbers(row_numbers)
 # p "Missing from cols: #{missing_numbers(col_numbers)}"
-col_duplicates = duplicate_numbers(col_numbers)
+# col_duplicates = duplicate_numbers(col_numbers)
 # p "Missing from dias: #{missing_numbers(diagonal_numbers)}"
 # p "Duplicates from dias: #{duplicate_numbers(diagonal_numbers)}"
-diagonal_duplicates = duplicate_numbers(diagonal_numbers)
+# diagonal_duplicates = duplicate_numbers(diagonal_numbers)
+
+# s = [[4, 8, 2],
+#      [4, 5, 7],
+#      [6, 2, 6]]
 
 # Find indices for duplicates
-p s[0].index()
-i = 0
-while i < s.length
-  j = 0
-  while j < s.length
-    if s[i][j] == row_duplicates[i]
-      p s[i][j]
-      p s[i].index(row_duplicates[i])
-    end
-    j += 1
-  end
-  i += 1
-end
+# i = 0
+# while i < s.length
+#   j = 0
+#   while j < s.length
+#     if s[i][j] == row_duplicates[i]
+#       # p s[i][j]
+#       # p s[i].index(row_duplicates[i])
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
 
 # p s[0].index(2)
 
